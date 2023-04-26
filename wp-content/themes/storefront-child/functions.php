4835eec04f5af254b44918f955ae8d014f222442
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-26 11:57:21 +0200

    remove sidebar from hook

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -9,3 +9,10 @@ function add_slider_frontpage()
 }
 
 add_action('storefront_before_content', 'add_slider_frontpage');
+
+function remove_storefront_sidebar()
+{
+    remove_action('storefront_sidebar', 'storefront_get_sidebar', 10);
+}
+
+add_action('get_header', 'remove_storefront_sidebar');
