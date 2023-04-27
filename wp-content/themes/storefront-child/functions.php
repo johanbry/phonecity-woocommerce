15eb0a93b66e74dcdffa709fe9ce2dd2f02b7f68
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-04-27 16:12:00 +0200

    removed featured image from post before ontent hook

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -14,5 +14,11 @@ function remove_storefront_sidebar()
 {
     remove_action('storefront_sidebar', 'storefront_get_sidebar', 10);
 }
-
 add_action('get_header', 'remove_storefront_sidebar');
+
+function remove_storefront_featuredimage()
+{
+    remove_action('storefront_post_content_before', 'storefront_post_thumbnail', 10);
+}
+
+add_action('get_header', 'remove_storefront_featuredimage');
