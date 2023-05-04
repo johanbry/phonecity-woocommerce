d93c1227acff9e993f4982282e2e8f31088b2bf2
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-05-04 08:33:54 +0200

    added plugin file with name and description

---
+++ wp-content/plugins/phonecity_shipping_method/phonecity_shipping_method.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: PhoneCity Shipping Method
+Description: PhoneCity Shipping method lets you specify flat rate with additional costs for shipping classes, weight and distance to customer.
+Version: 1.0.0
+Author: Grupp 2
+*/
